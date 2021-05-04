51e450f0403e70dfe7dcccbae9092c9f5902c1c9
portus-zephyr <polarisinc1056@gmail.com>
2021-05-04 16:38:19 +0800

    add Ossu pye exercise files

---
+++ OSSU/pye-2.py
@@ -0,0 +1,33 @@
+# examples
+
+
+# activities
+
+# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+# name = input("Enter your name: ")
+# print("Hello " + name)
+
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+# hours = float(input("Enter Hours: "))
+# rate = float(input("Enter Rate: "))
+# pay =  hours * rate
+# print("Pay: ", pay)
+
+# Exercise 4: Assume that we execute the following assignment statements:
+# width = 17
+# height = 12.0
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+
+# 1. width//2 
+# 2. width/2.0
+# 3. height/3
+# 4. 1 + 2 * 5
+# 8 8.5 4.0 11
+
+# Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+print("|--Celsius to Fahrenheit Converter--|")
+celsius = float(input("Celsius: "))
+fahrenheit = (celsius * (9/5)) + 32
+print("In Fahrenheit: ", fahrenheit)
