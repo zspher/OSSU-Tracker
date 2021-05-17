19feaed030916298f6b106b8bd1eb85d0d3a8e74
portus-zephyr <you@example.com>
2021-05-17 18:14:43 +0800

    finished pye 7

--- OSSU/python-for-everybody/pye-7.py
+++ OSSU/python-for-everybody/pye-7.py
@@ -27,18 +27,18 @@
 # Average spam confidence: 0.750718518519
 # Test your file on the mbox.txt and mbox-short.txt files.
 
-fname = input('Enter the file name: ')
-fhand = open(fname)
-count = 0
-spam = 0.0
-for line in fhand:
-  if line.startswith("X-DSPAM-Confidence: "):
-    count += 1
-    start = line.find("X-DSPAM-Confidence: ")
-    length = len("X-DSPAM-Confidence: ")
-    end = line.find("\n", start)
-    spam += float(line[start+length:end])
-print("Average spam confidence:", spam/count)
+# fname = input('Enter the file name: ')
+# fhand = open(fname)
+# count = 0
+# spam = 0.0
+# for line in fhand:
+#   if line.startswith("X-DSPAM-Confidence: "):
+#     count += 1
+#     start = line.find("X-DSPAM-Confidence: ")
+#     length = len("X-DSPAM-Confidence: ")
+#     end = line.find("\n", start)
+#     spam += float(line[start+length:end])
+# print("Average spam confidence:", spam/count)
 
 # Exercise 3: Sometimes when programmers get bored or want to have a bit of fun, they add a harmless Easter Egg to their program. Modify the program that prompts the user for the file name so that it prints a funny message when the user types in the exact file name “na na boo boo”. The program should behave normally for all other files which exist and don’t exist. Here is a sample execution of the program:
 
@@ -54,3 +54,20 @@ print("Average spam confidence:", spam/count)
 # Enter the file name: na na boo boo
 # NA NA BOO BOO TO YOU - You have been punk'd!
 # We are not encouraging you to put Easter Eggs in your programs; this is just an exercise.
+
+fname = input('Enter the file name: ')
+try:
+  fhand = open(fname)
+except:
+  if (fname == "na na boo boo"): 
+    print("NA NA BOO BOO TO YOU - You have been punk'd!")
+  else:
+    print("File cannot be opened: ", fname)
+  exit()
+
+count = 0
+for line in fhand:
+  count += 1
+
+print("There were", count, "subject lines in", fname)
+
