9b1c4eb63aa5922d1f2636d81a7237f4e8178e8d
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2021-06-14 19:02:04 +0800

    test4

--- OSSU/python-for-everybody/pye-12.py
+++ OSSU/python-for-everybody/pye-12.py
@@ -48,6 +48,8 @@ while True:
 print('\n')
 print(count)
 a_socket.close()
+
+
 # Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL, (2) displaying up to 3000 characters, and (3) counting the overall number of characters in the document. Don’t worry about the headers for this exercise, simply show the first 3000 characters of the document contents.
 
 import urllib.request 
