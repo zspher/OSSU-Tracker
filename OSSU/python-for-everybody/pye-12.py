14fcce56f1e3a01ca92945789733f94a9be5d6d7
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2021-06-16 15:57:33 +0800

    added pye ch 13 act 4-5 & quiz

--- OSSU/python-for-everybody/pye-12.py
+++ OSSU/python-for-everybody/pye-12.py
@@ -74,4 +74,43 @@ print(count)
 
 # Exercise 4: Change the urllinks.py program to extract and count paragraph (p) tags from the retrieved HTML document and display the count of the paragraphs as the output of your program. Do not display the paragraph text, only count them. Test your program on several small web pages as well as some larger web pages.
 
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+links = re.findall(b'(</p>)', html)
+count = len(links)
+print(count)
+
 # Exercise 5: (Advanced) Change the socket program so that it only shows data after the headers and a blank line have been received. Remember that recv receives characters (newlines and all), not lines.
+import sys
+import socket
+
+url = input("Enter a Url txt file: ")
+try:
+  website = url.split('/')[2]
+  a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  a_socket.connect((website, 80))
+  a_socket.send(f'GET {url} HTTP/1.0\r\n\r\n'.encode())
+except:
+  print("improperly formatted or non-existent URL")
+  sys.exit()
+
+file = b''
+while True:
+  data = a_socket.recv(512)
+  if len(data) < 1: break
+  file += data
+
+a_socket.close()
+
+pos = file.find(b'\r\n\r\n')
+file = file[pos+4:].decode()
+print(file)
---
+++ OSSU/python-for-everybody/pye-12q.py
@@ -0,0 +1,114 @@
+# Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_934760.html (Sum ends with 17)
+# You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+# Data Format
+# The file is a table of names and comment counts. You can ignore most of the data in the file except for lines like the following:
+
+# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+# <tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+# <tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
+# You are to find all the <span> tags in the file and pull out the numbers from the tag and sum the numbers.
+# Look at the sample code provided. It shows how to find all of a certain kind of tag, loop through the tags and extract the various aspects of the tags.
+
+# ...
+# # Retrieve all of the anchor tags
+# tags = soup('a')
+# for tag in tags:
+#    # Look at the parts of a tag
+#    print 'TAG:',tag
+#    print 'URL:',tag.get('href', None)
+#    print 'Contents:',tag.contents[0]
+#    print 'Attrs:',tag.attrs
+# You need to adjust this code to look for span tags and pull out the text content of the span tag, convert them to integers and add them up to complete the assignment.
+# Sample Execution
+
+# $ python3 solution.py
+# Enter - http://py4e-data.dr-chuck.net/comments_42.html
+# Count 50
+# Sum 2...
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup(class_ = "comments")
+sum = 0
+for tag in tags:
+   sum += int(tag.contents[0]) 
+
+print(sum)
+
+# Following Links in Python
+
+# In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
+
+# We provide two files for this assignment. One is a sample file where we give you the name for your testing and the other is the actual data you need to process for the assignment
+
+# Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+# Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+# Last name in sequence: Anayah
+# Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Virginie.html
+# Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+# Hint: The first character of the name of the last page that you will load is: R
+# Strategy
+# The web pages tweak the height between the links and hide the page after a few seconds to make it difficult for you to do the assignment without writing a Python program. But frankly with a little effort and patience you can overcome these attempts to make it a little harder to complete the assignment without writing a Python program. But that is not the point. The point is to write a clever Python program to solve the program.
+
+# Sample execution
+
+# Here is a sample execution of a solution:
+
+# $ python3 solution.py
+# Enter URL: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Enter count: 4
+# Enter position: 3
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Montgomery.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Mhairade.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Butchi.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Anayah.html
+# The answer to the assignment for this execution is "Anayah".
+
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# functions
+def getSoup(url, ctx):
+  html = urllib.request.urlopen(url, context=ctx).read()
+  return BeautifulSoup(html, 'html.parser')
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+count = int(input('Enter count: '))
+pos = int(input('Enter position: '))
+
+print('Retrieving: ', url)
+
+for i in range(count):
+  tags = getSoup(url, ctx).find_all('a')
+  for i, tag in enumerate(tags, start=1):
+    if i == pos:
+      url = tag.get('href', None)
+      print('Retrieving: ', url)
+      break
