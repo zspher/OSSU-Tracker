9efd919ae7f4bddc7057052de44922b5c68f3c5b
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2022-06-09 23:39:56 +0800

    add pset 4 problem a

--- Courses/Intro to Comp Sci/ps4/ps4a.py
+++ Courses/Intro to Comp Sci/ps4/ps4a.py
@@ -22,19 +22,45 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    if len(sequence) == 1:
+        return [sequence]
 
-    pass #delete this line and replace with your code here
+    c1 = sequence[0] # first letter of sequence
+    perm_list = []   
+    for p in get_permutations(sequence[1:]): # everything else
+        for i in range(len(sequence)):
+            perm_list.append( p[:i] + c1 + p[i:] ) # insert at different places
+
+    return perm_list
+    # i'm dumb, why did this take so long
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
+    #    #EXAMPLE
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+
 #    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
+#    to be three characters or fewer as you will have n! permutations for a
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    print('\n===== Test =====')
 
+    print('\n3 chars')
+    example_input = 'def'
+    print('Input:', example_input)
+    print('Expected Output:', ['def', 'dfe', 'edf', 'efd', 'fed', 'fde'])
+    print('Actual Output:', get_permutations(example_input))
+
+    print('\n2 chars')
+    example_input = 'ab'
+    print('Input:', example_input)
+    print('Expected Output:', ['ba', 'ab'])
+    print('Actual Output:', get_permutations(example_input))
+
+    print('\n1 char')
+    example_input = 'a'
+    print('Input:', example_input)
+    print('Expected Output:', ['a'])
+    print('Actual Output:', get_permutations(example_input))
